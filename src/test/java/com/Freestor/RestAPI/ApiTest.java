a145f88947d111387c63aa5864bc45c901968ff8
imdadul2005 <hoq.imdadul@gmail.com>
2018-01-19 20:52:04 -0500

    Added log4j2 to keep log record in logs folder.

--- src/test/java/com/Freestor/RestAPI/ApiTest.java
+++ src/test/java/com/Freestor/RestAPI/ApiTest.java
@@ -1,9 +1,15 @@
 package com.Freestor.RestAPI;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
+
 import static com.Freestor.RestAPI.CommonAPI.*;
+import static io.restassured.RestAssured.given;
 
 import java.io.IOException;
 
@@ -16,15 +22,16 @@ public class ApiTest {
     @Test
     public void apiTest() throws IOException {
 
-        int runTime = runTime();
-        JsonPath jsonRespond =null;
+        int runTime = CommonAPI.runTime();
+        JsonPath jsonRespond = null;
         int start = 0;
+
         while(start!=runTime) {
             if(type().equalsIgnoreCase("post"))
                 jsonRespond = commonPost(uri(), getPayLoadFile());
             if (type().equalsIgnoreCase("get"))
                 jsonRespond= commonGet(uri());
-            jsonRespond.prettyPrint();
+           //  jsonRespond.prettyPrint();
             start++;
         }
     }
